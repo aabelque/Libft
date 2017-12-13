8cba9483b529b435248c1b8ed94eea1cf6761f49
aabelque <34238994+aabelque@users.noreply.github.com>
2017-12-13 09:56:41 +0100

    Add files via upload

---
+++ ft_strjoin.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/27 12:36:06 by aabelque          #+#    #+#             */
+/*   Updated: 2017/11/30 17:08:36 by aabelque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char *str;
+
+	if (!s1 || !s2)
+		return (NULL);
+	if (!(str = ft_strnew(ft_strlen(s1) + ft_strlen(s2))))
+		return (NULL);
+	if (str != '\0')
+	{
+		ft_strcpy(str, s1);
+		ft_strcat(str, s2);
+	}
+	return (str);
+}
