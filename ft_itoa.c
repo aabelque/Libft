fbe831de53fa67bebab2a08b6346c7ad8800ba4f
aabelque <aabelque@e3r2p18.42.fr>
2018-04-19 15:39:40 +0200

    add itoa_base.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 18:03:02 by aabelque          #+#    #+#             */
-/*   Updated: 2017/11/30 16:35:07 by aabelque         ###   ########.fr       */
+/*   Updated: 2018/04/02 13:37:36 by aabelque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,42 +19,41 @@ static	int		ft_size_number(int n)
 
 	count = 0;
 	if (n < 0)
-	{
-		n *= -1;
 		count++;
-	}
 	if (n == 0)
 		return (1);
-	while (n > 0)
+	while (n != 0)
 	{
-		n = n / 10;
+		n /= 10;
 		count++;
 	}
 	return (count);
 }
+
+static int		ft_abs(int val)
+{
+	return (val < 0 ? -val : val);
+}
+
 
 char			*ft_itoa(int n)
 {
 	char	*str;
 	size_t	len;
 
-	if (n < -2147483647)
-		return (ft_strdup("-2147483648"));
 	len = ft_size_number(n);
 	if (!(str = (char *)malloc(sizeof(*str) * len + 1)))
 		return (NULL);
 	if (n < 0)
-	{
 		str[0] = '-';
-		n *= -1;
-	}
 	str[len] = '\0';
-	while (n >= 10)
+	while (1)
 	{
-		str[len - 1] = n % 10 + '0';
-		n = n / 10;
+		str[len - 1] = ft_abs(n % 10) + '0';
+		n /= 10;
+		if (n == 0)
+			break ;
 		len--;
 	}
-	str[len - 1] = n + '0';
 	return (str);
 }
